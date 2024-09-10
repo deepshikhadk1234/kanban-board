04af01a5b80b5d7c3bc767782864025e993dc61f
Deepshikha <deepshikhadk1234@gmail.com>
2024-09-10 18:48:07 +0530

    reorg

---
+++ src/components/TaskCard.tsx
@@ -0,0 +1,21 @@
+const Taskcard = ({ task }: {
+  task: {
+  title: string,
+  id: string,
+  points?: number
+  }
+}) => {
+  return (
+    <div className="border rounded-lg p-2 m-2 px-5 bg-white">
+      <div className="text-base font-semibold py-2">
+        {task.title}
+      </div>
+      <div className="flex gap-4 justify-between py-2 text-gray-500">
+        <div>{task.id}</div>
+        <div>{task.points}</div>
+      </div>
+    </div>
+  )
+}
+
+export default Taskcard

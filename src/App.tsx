6e5220455e42391d8825facf7f3751be0ad2e766
Deepshikha <deepshikhadk1234@gmail.com>
2024-09-10 19:03:27 +0530

    added arrays

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,15 @@
 
 import './App.css'
 import TaskCard from './components/TaskCard.tsx'
-import { Task } from './utils/data-task'
-
-
-
+import { tasks } from './utils/data-task'
 
 function App() {
-  // const [count, setCount] = useState(0)
-  const task: Task={
-    title: 'Kanban Board',
-    id: '1',
-    points: 10
-  }
-
+  
   return (
     <>
-    <TaskCard task={task} />
+    {tasks.map((task) => (
+      <TaskCard task={task} />
+    ))}
    
     </>
     
--- src/utils/data-task.ts
+++ src/utils/data-task.ts
@@ -3,3 +3,26 @@ export type Task = {
     id: string
     points?: number
   }
+
+  export const tasks: Task[] = [
+    {
+      title: 'Kanban Board',
+      id: '1',
+      points: 10
+    },
+    {
+      title: 'Kanban Board',
+      id: '2',
+      points: 334
+    },
+    {
+      title: 'Kanban Board',
+      id: '3',
+      points: 33
+    },
+    {
+      title: 'Kanban Board',
+      id: '4',
+      points: 90
+    }
+  ]

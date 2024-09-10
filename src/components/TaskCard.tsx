ef2cd4413caf8b7522b282974bdf9ba405652888
Deepshikha <deepshikhadk1234@gmail.com>
2024-09-10 19:53:13 +0530

    added icons

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,5 +1,16 @@
 import { Task } from '../utils/data-task'
 
+const lowPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 9l7 7 7-7" />
+</svg>
+const mediumPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10h14" />
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 14h14" />
+</svg>
+const highPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
+</svg>
+
 const Taskcard = ({ task }: {
   task: Task
 }) => {
@@ -12,7 +23,13 @@ const Taskcard = ({ task }: {
         {task.description}
       </div>
       <div className="flex gap-4 justify-between py-2 text-gray-500 text-sm">
-        <div>{task.id}</div>
+        <div className="flex gap-2">
+          <div>{task.id}</div>
+          <div>{task.priority}</div>
+          {task.priority === 'high' && highPriorityIcon}
+        {task.priority === 'medium' && mediumPriorityIcon}
+        {task.priority === 'low' && lowPriorityIcon}
+          </div>
         <div>{task.points}</div>
       </div>
     </div>
--- src/utils/data-task.ts
+++ src/utils/data-task.ts
@@ -1,20 +1,22 @@
 export type Status = 'todo' | 'inProgress' | 'done'
-
+export type Priority = 'low' | 'medium' | 'high'
 export type Task = {
     title: string
     id: string
     description?: string
     status: Status
+    priority: Priority
     points?: number
   }
  export const statuses: Status[]=['todo', 'inProgress', 'done']
-
+export const priorities: Priority[]= ['low','medium','high']
   export const tasks: Task[] = [
     {
       title: 'Kanban Board',
       id: '1',
       description: 'Hello how are you ????',
       status: 'todo',
+      priority: 'medium',
       points: 10
     },
     {
@@ -22,48 +24,56 @@ export type Task = {
       id: '2',
       description: 'Hello how are you ????',
       status: 'inProgress',
+      priority: 'medium',
       points: 334
     },
     {
       title: 'Kanban Board',
       id: '3',
       status: 'done',
+      priority: 'high',
       points: 33
     },
     {
       title: 'Kanban Board',
       id: '4',
       status: 'todo',
+      priority: 'medium',
       points: 90
     },
     {
       title: 'Kanban Board',
       id: '5',
       status: 'inProgress',
+      priority: 'low',
       points: 334
     },
     {
       title: 'Kanban Board',
       id: '6',
       status: 'done',
+      priority: 'medium',
       points: 33
     },
     {
       title: 'Kanban Board',
       id: '7',
       status: 'todo',
+      priority: 'high',
       points: 90
     },
     {
       title: 'Kanban Board',
       id: '8',
       status: 'inProgress',
+      priority: 'medium',
       points: 334
     },
     {
       title: 'Kanban Board',
       id: '9',
       status: 'done',
+      priority: 'low',
       points: 33
     }
 
